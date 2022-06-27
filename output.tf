85288f406131d00a937692b9c81cbdc4a10d8369
jjoshgarcia <87858027+jjoshgarcia@users.noreply.github.com>
2022-06-27 19:00:54 +0200

    chang local host

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 
 output "port" {
-  value = (local.do_tunnel ? data.external.free_port.result.port : var.target_port)
+  value = (local.do_tunnel ? var.local_port : var.target_port)
   description = "Port number to connect to"
 }
 
